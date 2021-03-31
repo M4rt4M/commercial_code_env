d1992bb1a73a12e56188e9b378242b02f30604ad
Chung <Chung224@live.co.uk>
2021-03-31 22:40:46 +0100

    moved files

---
+++ Unit_test/authentication_app_interaction.py
@@ -0,0 +1,52 @@
+
+import os
+import requests
+import json
+
+
+############################################################################################################################################
+############################################################################################################################################
+def get_token(): # define function to grab variables 
+    auth0_token_request ={ 
+        "audience":os.getenv('api_identifier'), # stored relevant details within own system, call variables into a dictionary for HTTP post attempt
+        "grant_type":"client_credentials",      #Post attempt done to grab token 
+        "client_id":os.getenv('client_id'),
+        "client_secret":os.getenv('secret')
+        }
+    
+    post_response = requests.post(os.getenv('auth_token_url'),data = auth0_token_request) #post the auth0_token_request dictionary  and grab reponse code
+    grab_info =  requests.post(os.getenv('auth_token_url'),data = auth0_token_request).json() #grab the data sent back including the token code
+    token_id = grab_info["access_token"] #use key-value pair to access token
+    
+    
+    #We raise an error here if the desired reponse is not achieved - let the user know of situation if so. 
+    if post_response.status_code <200:
+        print('Error obtaining value token') # we print instead of raising valuerror for the sake of alexa communication - I.E code keeps running
+    elif post_response.status_code >=400:    # and we notify user
+        print('Error obtaining value token')
+    return token_id,post_response.status_code
+    
+
+
+
+
+############################################################################################################################################
+############################################################################################################################################
+def grab_user_info(email, token_id): # we create a function that takes an email + token in order to grab information from Alfred's backend. 
+    http_headers = {"authorization": "Bearer " + token_id}
+    url = os.getenv('api_identifier') + "/profile/find-one?email=" + email
+    request_user_info = requests.get(url, headers=http_headers)
+    post_reponse = request_user_info.status_code
+    user_information = request_user_info.json()
+    if request_user_info.status_code <200:
+        print('Error obtaining value token') # we print instead of raising valuerror for the sake of alexa communication - I.E code keeps running
+    elif request_user_info.status_code >=400:    # and we notify user
+        print('Error obtaining value token')
+    return user_information,post_reponse
+
+
+
+
+
+    
+    
